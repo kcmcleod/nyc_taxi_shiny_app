9627bf8ed90e080f527d44b29ccaab1ec96b9280
Kenneth McLeod <mcleod.kc@gmail.com>
2026-07-28 18:19:40 +0100

    [feat] updated data with lookups

--- dataPrep/processYellowTaxiData.R
+++ dataPrep/processYellowTaxiData.R
@@ -1,26 +1,32 @@
+start_time = Sys.time()
+
 source(paste0(getwd(), "/global.R"))
 library(arrow)
 library(data.table)
 library(logger)
+library(prettyunits)
 
 sourceRFilesFromFolder(paste0(getwd(), "/dataPrep/yellow_taxi_helpers"))
 
+################################################################################
 # SETUP LOGGING
-log_appender(appender_tee(paste0(dataPath, "logs/", format(Sys.time(), "%Y%m%d_%H%M"), "_yellow.log")))
+log_name <- paste0(dataPath, "logs/", format(Sys.time(), "%Y%m%d_%H%M"), "_yellow.log")
+log_appender(appender_tee(log_name))
 log_threshold(INFO)
 log_info("STARTING TO PROCESS YELLOW DATA")
 
-yellowFiles <- list.files(path = paste0(dataPath, "src/"), pattern = "yellow.*.parquet", 
+yellowFiles <- list.files(path = paste0(dataPath, "src"), pattern = "yellow.*.parquet", 
                           recursive = TRUE, full.names = TRUE)
 
 if(length(yellowFiles) == 0) {
-   log_error("Cannot find any files to process")
+  log_error("Cannot find any files to process")
   return(NULL)
 }
 
 log_info("Found ", length(yellowFiles), " files to process...")
 
-# objects built in the loop
+################################################################################
+# LOOP THRU DATA FILES
 combinedDF <- NULL
 expected_cols <- NULL
 
@@ -44,12 +50,38 @@ for(file in yellowFiles) {
   }
 }
 
-# write generated data to disk
+################################################################################
+# SORT
 sort_cols <- c("full_month_aggregation", "full_week_aggregation", "date")
 combinedDF <- arrange(combinedDF, across(all_of(sort_cols)))
 
+################################################################################
+# LOOKUPS
+zone_lookups <- read_csv(paste0(dataPath, "src/taxi_zone_lookup.csv")) |> 
+  select("LocationID", "Borough")
+rate_lookups <- read_csv(paste0(dataPath, "src/rate_code_lookup.csv"), col_types = c("ic"))
+payment_lookups <- read_csv(paste0(dataPath, "src/payment_type_lookup.csv"), col_types = c("ic"))
+vendor_lookups <- read_csv(paste0(dataPath, "src/vendor_lookup.csv"), col_types = c("ic"))
+
+combinedDF <- fn_perform_lookup(combinedDF, rate_lookups, "RatecodeID", "RatecodeID", "Rate", "rate") |> 
+  fn_perform_lookup(zone_lookups, c("PULocationID" = "LocationID"), "PULocationID", "PULocation", "PU",
+                    post_process_fn = function(df) rename(df, PULocation = Borough)) |> 
+  fn_perform_lookup(zone_lookups, c("DOLocationID" = "LocationID"), "DOLocationID", "DOLocation", "DO", 
+                    post_process_fn = function(df) rename(df, DOLocation = Borough)) |> 
+  fn_perform_lookup(payment_lookups, "payment_type", "payment_type", "payment_type", "payment", 
+                    post_process_fn = function(df) mutate(df, payment_type = payment_class)) |> 
+  fn_perform_lookup(vendor_lookups, "VendorID", "VendorID", "Vendor", "vendor")
+
+################################################################################
+# WRITE ASSET
 write_parquet(combinedDF, 
               sink = paste0(dataPath, format(Sys.Date(), "%Y%m%d"), "_yellow_aggregation.parquet"))
 
 remove(combinedDF)
 gc()
+
+total_time = Sys.time() - start_time
+pretty_duration <- pretty_ms(as.numeric(total_time, units = "secs") * 1000)
+log_info("Finished! This took: ", pretty_duration)
+message("Log: ", str_replace_all(string = log_name, pattern = " ", replacement = "\\\\ "))
+message("GOOD BYE!")
---
+++ dataPrep/yellow_taxi_helpers/fn_perform_lookup.R
@@ -0,0 +1,31 @@
+# Helper function to handle join and validation safely
+fn_perform_lookup <- function(df, lookup_df, join_by_cols, check_col_pre, check_col_post, label, post_process_fn = NULL) {
+  
+  # data checks
+  if (!is.data.frame(df)) {
+    log_error("Invalid dataframe passed to perform_lookup for {label}.")
+    stop("Input 'df' must be a data frame.")
+  }
+  
+  # actual joins
+  og_na_count <- sum(is.na(df[[check_col_pre]]))
+  
+  # Perform join
+  df <- left_join(df, lookup_df, by = join_by_cols)
+  
+  # Optional post-processing (e.g., renaming or overriding columns)
+  if (!is.null(post_process_fn)) {
+    df <- post_process_fn(df)
+  }
+  
+  new_na_count <- sum(is.na(df[[check_col_post]]))
+  
+  # Log results safely
+  if (new_na_count == og_na_count) {
+    log_info(".... no new NAs introduced in {label} data. We have: {og_na_count}")
+  } else {
+    log_error(".... new NAs introduced in {label} data. We have {og_na_count} vs {new_na_count}")
+  }
+  
+  return(df)
+}
