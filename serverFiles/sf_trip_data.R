4526521f374cfd6091a5a9fb4d722dbbbd6feda9
Kenneth McLeod <mcleod.kc@gmail.com>
2026-07-31 10:01:38 +0100

    [feat] upgrading to validate/need

--- serverFiles/sf_trip_data.R
+++ serverFiles/sf_trip_data.R
@@ -1,10 +1,7 @@
 
 
 observeEvent(yellow_taxi_data(), {
-  
-  if(is.null(yellow_taxi_data())) 
-    return(NULL)
-  
+
   tDF <- yellow_taxi_data()
   vendor_values = sort(unique(tDF$VendorID))
   payment_types = sort(unique(tDF$payment_type))
@@ -27,54 +24,57 @@ observeEvent(yellow_taxi_data(), {
 
 # main trip volumes chart
 output$po_tripVolumesOverTime <- renderPlotly({
-  req(yellow_taxi_data(), input$pi_vendor, input$pi_pay_type, 
-      input$rb_journeys_or_passengers)
+  req(yellow_taxi_data(), input$rb_journeys_or_passengers)
   
-  if(! is.null(yellow_taxi_data())) {
-    
-    month_agg <- ifelse(input$pi_level == "Month", TRUE, FALSE)
-    week_agg <- ifelse(input$pi_level == "Week", TRUE, FALSE)  
-    data_field <- ifelse(input$rb_journeys_or_passengers == "Total Distance", 
-                         "total_distance", "total_number_trips")
-    
-    x_lab_format <- switch (
-      input$pi_level,
-      "Month" = "%b %Y",
-      "%d %b %Y"
-    )
-    
-    y_lab_title <- ifelse(input$rb_journeys_or_passengers == "Total Distance",
-        "Distance in miles", "Total number of trips"  
-    )
-
-    tmpDF <- yellow_taxi_data() |> 
-      filter(
-        full_month_aggregation == month_agg,
-        full_week_aggregation == week_agg,
-        VendorID %in% input$pi_vendor,
-        payment_type %in% input$pi_pay_type
-        ) |> 
-      select(date, data_field) |> 
-      group_by(date) |> 
-      summarise(total_value = sum(.data[[data_field]], na.rm = TRUE), .groups = 'drop') 
-    
-    fig <- plot_ly(tmpDF, x = ~date, y = ~total_value, type = 'scatter', mode = 'line') |> 
-      layout(
-        title = "Yellow Taxi journeys",
-        xaxis = list(
-          title = "Journey month",
-          tickmode = "array",
-          tickvals = tmpDF$date,
-          ticktext = format(tmpDF$date, x_lab_format)
-        ),
-        yaxis = list(
-          title = y_lab_title
-        )
+  # User-facing validations for interactive selections
+  validate(
+    need(length(input$pi_vendor) > 0, "Please select at least one vendor."),
+    need(length(input$pi_pay_type) > 0, "Please select at least one payment type."),
+    need(length(input$pi_level) == 1, "Please select exactly one payment type.")
+  )
+  
+  month_agg <- ifelse(input$pi_level == "Month", TRUE, FALSE)
+  week_agg <- ifelse(input$pi_level == "Week", TRUE, FALSE)  
+  data_field <- ifelse(input$rb_journeys_or_passengers == "Total Distance", 
+                       "total_distance", "total_number_trips")
+  
+  x_lab_format <- switch (
+    input$pi_level,
+    "Month" = "%b %Y",
+    "%d %b %Y"
+  )
+  
+  y_lab_title <- ifelse(input$rb_journeys_or_passengers == "Total Distance",
+                        "Distance in miles", "Total number of trips"  
+  )
+  
+  tmpDF <- yellow_taxi_data() |> 
+    filter(
+      full_month_aggregation == month_agg,
+      full_week_aggregation == week_agg,
+      VendorID %in% input$pi_vendor,
+      payment_type %in% input$pi_pay_type
+    ) |> 
+    select(date, data_field) |> 
+    group_by(date) |> 
+    summarise(total_value = sum(.data[[data_field]], na.rm = TRUE), .groups = 'drop') 
+  
+  fig <- plot_ly(tmpDF, x = ~date, y = ~total_value, type = 'scatter', mode = 'line') |> 
+    layout(
+      title = "Yellow Taxi journeys",
+      xaxis = list(
+        title = "Journey month",
+        tickmode = "array",
+        tickvals = tmpDF$date,
+        ticktext = format(tmpDF$date, x_lab_format)
+      ),
+      yaxis = list(
+        title = y_lab_title
       )
-    
-    remove(tmpDF)
-    
-    return(fig)    
-    
-  }
+    )
+  
+  remove(tmpDF)
+  
+  return(fig)    
+  
 })
