ca682fc5487f6d8599217c079ea700649a8104f1
Kenneth McLeod <mcleod.kc@gmail.com>
2026-07-31 10:40:17 +0100

    [feat] upgrade shinytest2 script

---  scripts/create_shinytest2_tests.R
+++  scripts/create_shinytest2_tests.R
@@ -1,10 +1,28 @@
 library(shinytest2)
 
+################################################################################
+# SETUP
+
 # Sets up the necessary infrastructure and 'tests/testthat/' folder
 use_shinytest2()
+
+
+
+################################################################################
+# CREATE TESTS
 
 # create tests
 record_test()
 
+# if snaps get really large, eg MBs, switch from app$expect_values() to app$get_value()
+# plotly_data <- app$get_value(output = "po_tripVolumesOverTime")
+# expect_true(length(plotly_data$x$data) > 0) 
+
 # run tests
 test_app()
+
+
+
+################################################################################
+# UPDATE SNAPS
+testthat::snapshot_accept()
