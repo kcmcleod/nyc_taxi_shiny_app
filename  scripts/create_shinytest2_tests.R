0c25da37f2cd6b4846e3fa4047fc18e9a50855a1
Kenneth McLeod <mcleod.kc@gmail.com>
2026-07-31 10:02:05 +0100

    [feat] shinytest2

---
+++  scripts/create_shinytest2_tests.R
@@ -0,0 +1,10 @@
+library(shinytest2)
+
+# Sets up the necessary infrastructure and 'tests/testthat/' folder
+use_shinytest2()
+
+# create tests
+record_test()
+
+# run tests
+test_app()
---
+++ tests/testthat/test-shinytest2.R
@@ -0,0 +1,24 @@
+library(shinytest2)
+
+test_that("{shinytest2} recording: main_chart_month_vendor1_cash", {
+  app <- AppDriver$new(test_path("../.."), name = "main_chart_month_vendor1_cash", 
+      height = 928, width = 1619)
+  app$set_inputs(pi_vendor = "1")
+  app$set_inputs(pi_pay_type = "Cash")
+  app$set_inputs(pi_level = "Month")
+  app$expect_values(output = "po_tripVolumesOverTime")
+})
+
+
+test_that("{shinytest2} recording: main_chart_month_vendor6_flex_flare_distance", {
+  app <- AppDriver$new(test_path("../.."), name = "main_chart_month_vendor6_flex_flare_distance", 
+      height = 928, width = 1619)
+  app$set_inputs(pi_level = "Month")
+  app$set_inputs(pi_vendor = "6")
+  app$set_inputs(pi_pay_type = "Dispute")
+  app$set_inputs(pi_pay_type = "Credit card")
+  app$set_inputs(pi_pay_type = "Cash")
+  app$set_inputs(pi_pay_type = "Flex Fare trip")
+  app$set_inputs(rb_journeys_or_passengers = "Total Distance")
+  app$expect_values()
+})
