c81fbd9d9614af7be374113595ae990c23d2d84c
Kenneth McLeod <mcleod.kc@gmail.com>
2026-08-04 16:59:17 +0100

    [fix] refactored UI and switched default view to month (not daily) so updating tests

--- tests/testthat/test_sf_trip_data.R
+++ tests/testthat/test_sf_trip_data.R
@@ -7,12 +7,12 @@ test_that("sf_trip_data - main chart", {
   app$wait_for_idle()
   
   # 1. Test the default state
-  expect_equal(app$get_value(export = "exported_filtered_rows"), 151)
+  expect_equal(app$get_value(export = "exported_filtered_rows"), 14)
 
   # 2. not default
   app$set_inputs(pi_level = "Month")
   app$set_inputs(pi_vendor = "Myle Technologies Inc")
   app$set_inputs(pi_pay_type = "Flex Fare trip")
   
-  expect_equal(app$get_value(export = "exported_filtered_rows"), 5)
+  expect_equal(app$get_value(export = "exported_filtered_rows"), 3)
 })
