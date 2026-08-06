dc242dd9acfaa488268d3b06d980c6f21ad02655
Kenneth McLeod <mcleod.kc@gmail.com>
2026-08-06 19:54:03 +0100

    [feat] adding coverage

---  scripts/create_shinytest2_tests.R
+++  scripts/create_shinytest2_tests.R
@@ -30,3 +30,23 @@ test_app()
 ################################################################################
 # UPDATE SNAPS
 testthat::snapshot_accept()
+
+
+
+################################################################################
+# COVERAGE
+
+app_sources <- list.files("R", pattern = "\\.[Rr]$", full.names = TRUE)
+app_tests <- list.files("tests/testthat", pattern = "\\.[Rr]$", full.names = TRUE)
+
+# filter out shinytest2 etc
+unit_tests <- grep("shinytest2|setup", app_tests, value = TRUE, invert = TRUE)
+core_sources <- grep("_disable", app_sources, value = TRUE, invert = TRUE)
+
+cov_results <- covr::file_coverage(
+  source_files = core_sources, 
+  test_files = unit_tests 
+)
+
+
+covr::report(cov_results)
