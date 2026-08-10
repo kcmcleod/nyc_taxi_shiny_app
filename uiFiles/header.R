106acac3e89b33df3b6252a372a76acb9e150471
Kenneth McLeod <mcleod.kc@gmail.com>
2026-08-10 13:40:34 +0100

    [ref] remove dead files

--- uiFiles/header.R
+++
@@ -1,7 +0,0 @@
-header <- dashboardHeader(
-  title = HTML(
-    "<img src='nyc_taxi_icon.png' height = '30px' alighn = 'left'>",
-    appVersion,
-    "style = 'vertical-align: -webkit-baseline-middle' >"
-  )
-)
