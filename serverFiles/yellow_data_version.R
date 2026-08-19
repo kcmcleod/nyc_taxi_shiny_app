0e4d59db42afd3b195764d549862a571227f3895
Kenneth McLeod <mcleod.kc@gmail.com>
2026-08-19 09:39:28 +0100

    [feat] simplifying the yellow_data_version reactive to remove i/o

--- serverFiles/yellow_data_version.R
+++ serverFiles/yellow_data_version.R
@@ -1,10 +1,7 @@
 # Create a reactive or global expression representing the data version
 # (e.g., using the file modification time of the latest parquet file)
 yellow_data_version <- reactive({
-  all_yellow_files <- list.files(dataPath, pattern = "yellow.*parquet", full.names = TRUE)
-  latest_file <- sort(all_yellow_files, decreasing = TRUE)[1]
-  if (is.na(latest_file)) {
-    return(0)
-  }
-  file.info(latest_file)$mtime
+  req(app_metadata, app_metadata$min_date, app_metadata$max_date)
+
+  paste0(app_metadata$min_date, "_", app_metadata$max_date)
 })
