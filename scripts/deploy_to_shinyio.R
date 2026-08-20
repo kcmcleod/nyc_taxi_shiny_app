5c943715e8643b2cb9bbd33937e8c2ca0c692aff
Kenneth McLeod <mcleod.kc@gmail.com>
2026-08-20 10:14:43 +0100

    [feat] script to deploy app to shinyapps.io

---
+++ scripts/deploy_to_shinyio.R
@@ -0,0 +1,20 @@
+file.rename("renv.lock", "renv_hidden.lock")
+
+rsconnect::deployApp()
+
+rsconnect::deployApp(
+  appFiles = c(
+    "app.R",
+    "global.R",
+    ".Renviron",
+    ".Rprofile",
+    "config",
+    "app_data",
+    "serverFiles",
+    "R",
+    "www",
+    "data"
+  )
+)
+
+file.rename("renv_hidden.lock", "renv.lock")
