26276a3459b6fc55afe4436812b9c15b44eb3f3b
Kenneth McLeod <mcleod.kc@gmail.com>
2026-08-23 12:54:45 +0100

    [fix] GH test fail due to missing shiny lib

--- tests/testthat/test_mod_info_page.R
+++ tests/testthat/test_mod_info_page.R
@@ -1,3 +1,6 @@
+library(testthat)
+library(shiny)
+
 test_that("mod_info_page_server initializes correctly", {
   testServer(mod_info_page_server, args = list(), {
     # If the module has no reactive outputs, just expecting it to run without error is enough.
