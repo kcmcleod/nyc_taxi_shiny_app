a4ddaf38b0bb348982227b08ca0ccd851834679b
Ken McLeod <kcmcleod@users.noreply.github.com>
2026-09-10 11:18:06 +0100

    Refactor repo (#12)
    * [feat] stop user selecting full date range and meekly line chart in availibility tab
    
    * [refactor] move ETL into final location
    
    * [ref] removing serverFiles folder and embedding code in server fun
    
    * [feat] switching to monthly heatmaps to make tab load faster
    
    * [ref] rename scripts folder as dev
    
    * [ref] moving sample data creation next to baseline script
    
    * [fix] updating tests and test setup so it works with makefile
    
    * [feat] adding makefile
    
    * [ref] removing unwanted snaps
    
    * [fix] resinstating snaps that were deleted
    
    * [fix] correct makefile command for testing

---
+++ Makefile
@@ -0,0 +1,19 @@
+.PHONY: sync test prune download poll coverage
+
+sync:
+	Rscript dev/sync_data_to_s3.R
+
+test:
+	Rscript -e "Sys.setenv(NOT_CRAN = 'true'); offline_data_prep <- TRUE; shinytest2::test_app()"
+
+prune:
+	Rscript dev/clean_local_git.R
+	
+download:
+	Rscript dev/download_code_for_ai.R
+	
+poll:
+	Rscript dev/data_and_data_prep/polling_nyc_for_data.R
+	
+coverage:
+	Rscript dev/testing/coverage.R
--- dev/testing/create_shinytest2_tests.R
+++ dev/testing/create_shinytest2_tests.R
@@ -33,21 +33,21 @@ testthat::snapshot_review()
 ################################################################################
 # COVERAGE
 
-app_sources <- list.files("R", pattern = "\\.[Rr]$", full.names = TRUE)
-dp_sources <- list.files("dataPrep/yellow_taxi_helpers", pattern = "\\.[Rr]$", full.names = TRUE)
-app_sources <- c(app_sources, dp_sources)
-app_tests <- list.files("tests/testthat", pattern = "\\.[Rr]$", full.names = TRUE)
-
-# filter out shinytest2 etc
-unit_tests <- grep("shinytest2|setup", app_tests, value = TRUE, invert = TRUE)
-core_sources <- grep("_disable", app_sources, value = TRUE, invert = TRUE)
-
-source("tests/testthat/setup.R")
-
-cov_results <- covr::file_coverage(
-  source_files = core_sources,
-  test_files = unit_tests
-)
-
-covr::report(cov_results)
+# app_sources <- list.files("R", pattern = "\\.[Rr]$", full.names = TRUE)
+# dp_sources <- list.files("data-raw/yellow_taxi_helpers", pattern = "\\.[Rr]$", full.names = TRUE)
+# app_sources <- c(app_sources, dp_sources)
+# app_tests <- list.files("tests/testthat", pattern = "\\.[Rr]$", full.names = TRUE)
+#
+# # filter out shinytest2 etc
+# unit_tests <- grep("shinytest2|setup", app_tests, value = TRUE, invert = TRUE)
+# core_sources <- grep("_disable", app_sources, value = TRUE, invert = TRUE)
+#
+# source("tests/testthat/setup.R")
+#
+# cov_results <- covr::file_coverage(
+#   source_files = core_sources,
+#   test_files = unit_tests
+# )
+#
+# covr::report(cov_results)
 covr::zero_coverage(cov_results)
